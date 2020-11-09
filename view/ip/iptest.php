c2dc7cedc6a6f182b915ef63826e8cced17ae5d6
Linnea <linnea.j.m.olofsson@gmail.com>
2020-11-09 10:09:34 +0100

    testbuttons added

--- view/ip/iptest.php
+++ view/ip/iptest.php
@@ -26,9 +26,11 @@ namespace Anax\View;
     <br>
     <input type="submit" class="submitbutton" value="Validera"></input>
 </form>
+<br>
 
 <div>
-    <p>Om du hellre vill validera din IP via din URL går det också bra.
+    <h3>JSON-validering</h3>
+    <p>Om du hellre vill validera din IP via din url går det också bra.
         <br>
         Detta gör du genom att skicka en GET-request, likt följande exempel: </p>
     <p><i> GET /ip-json?ip=216.58.211.142</i></p>
@@ -40,6 +42,30 @@ namespace Anax\View;
         "host": "arn09s10-in-f14.1e100.net"
     }
     </pre>
+</div>
+
+<div>
+    <h3>Testroutes</h3>
+    <p>Här nedan kan du testa routes för JSON-valideringen</p>
+    <form action=<?= url("ip-json")?>>
+        <br>
+        <input type="hidden" name="ip" value="216.58.211.142">
+        <input type="submit" class="JSONbutton right" value="Fungerande">
+    </form>
+    <form action=<?= url("ip-json")?>>
+        <br>
+        <input type="hidden" name="ip" value="216.58.21">
+        <input type="submit" class="JSONbutton wrong" value="Felaktig">
+    </form>
+    <br>
+    <form action=<?= url("ip-json")?>>
+        <label>Du kan även testa JSON-validerngen genom att skriva in en egen ip:</label>
+        <br>
+        <input type="text" name="ip">
+        <br>
+        <br>
+        <input type="submit" class="JSONbutton" value="Validera">
+    </form>
 </div>
 
 
